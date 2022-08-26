f009a52957e85067bf9089e9c35a0dc0e3f1f212
Jack-Cannon <cannonjackj@gmail.com>
2022-08-26 11:00:42 -0500

    adss github token

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const client = new ApolloClient({
   uri: 'https://api.github.com/graphql',
   cache: new InMemoryCache(),
   headers: {
-    "Authorization": `Bearer ${process.env.REACT_APP_BEARER_TOKEN}`
+    "Authorization": `Bearer ${process.env.GITHUB_TOKEN}`
   }
 });
 
