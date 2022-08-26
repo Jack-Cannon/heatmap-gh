5c0058a0def2c95c19b8ad19889af1896eddf078
Jack-Cannon <cannonjackj@gmail.com>
2022-08-26 09:58:31 -0500

    initial work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
+import CalendarHeatmap from 'react-calendar-heatmap';
+
+
+const GET_GRAPH = gql`
+query($userName:String!) { 
+  user(login: $userName){
+    contributionsCollection {
+      contributionCalendar {
+        totalContributions
+        weeks {
+          contributionDays {
+            contributionCount
+            date
+          }
+        }
+      }
+    }
+  }
+}
+`;
+
+
+function UserContributionGraph({userName}) {
+
+  const { loading, error, data } = useQuery(GET_GRAPH, {
+    variables: {userName: userName}
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  var processedData = []
+
+  var semiFlat = data.user.contributionsCollection.contributionCalendar.weeks
+  semiFlat.forEach(x => {
+    processedData = [...processedData, x.contributionDays]
+  })
+
+  var contributions = processedData.flat()
+
+  var contributionValues = contributions.map(x => 
+    x = {'date': x.date, 'count': x.contributionCount}
+  ).filter(x => x.count !== 0)
+
+  return (
+    <div>
+      <CalendarHeatmap
+        startDate={new Date(contributions[0].date)}
+        endDate={new Date(contributions[contributions.length -1].date)}
+        values={
+          contributionValues
+        }
+      />
+    </div>
+  ) 
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserContributionGraph userName='Jack-Cannon'/>
     </div>
   );
 }
